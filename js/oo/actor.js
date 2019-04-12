1b654fe80ad58b9f8b7c87010ba812744fb50239
Marty <marty.shu@gmail.com>
2019-04-12 09:12:48 +0800

    add switchImg

--- js/oo/actor.js
+++ js/oo/actor.js
@@ -1,11 +1,3 @@
-/*
-        var obj = new Actor({
-            'stage': 'c1',
-            'img': 'res/coin.png',
-            'snd': 'res/coin.mp3',
-            'pos': [100, 100, 25, 25]
-        })
-*/
 class Actor {
   constructor(cv, info) {
     var self = this;
@@ -13,28 +5,43 @@ class Actor {
     self.cv = cv;
     self.info = info;
     self.img = null;
+    self.imgReady = false;
+    self.touching = false;
+    this.lastInsideTime = -1;
     self.stage = info.stage; // camera
+    self.moveArray = [];
+    self.isFlip = self.stage.getFlip();
     self.body = document.getElementsByTagName('body')[0];
+    self.originImgURL = info.img;
+    self.originSize = [info.pos[3], info.pos[4]];
     self.setImg(info.img, info.pos);
     self.hide();
     self.audio = new Audio(info.snd);
-    self.moveArray = [];
     self.jsonInfo = {
       "history": 100,
       "varThreshold": 25,
       "learningRate": 0.001,
       "detectShadows": false,
-      "objMinSize": 5,
-      "filter": ["e3", "g1", "d3"]
+      "objMinSize": 3,
+      "touchTime": 1000,
+      "filter": ["e2", "g1", "d3"]
     };
-    self.onTouchCallback = function () {};
+    self.onTouchCallback = function () { };
     self.setTracking({
       'inside': function (pos) {
+        var nowTime = new Date().getTime();
+        if (nowTime - this.lastInsideTime < self.jsonInfo.touchTime) {
+          self.touching = true;
+          return;
+        }
+        self.touching = false;
+        this.lastInsideTime = nowTime;
         if (self.isHide()) return;
-        self.onTouchCallback();
+        self.inPos = pos;
+        self.onTouchCallback(pos);
       },
       'outside': function (pos) {
-        //console.log("outside");
+        self.outPos = pos;
       }
     });
   }
@@ -48,14 +55,30 @@ class Actor {
     this.tracking.scan();
   }
 
+  switchImg(url, switchTime) {
+    var self = this;
+    if (self.touching) return;
+    self.jsonInfo.touchTime = switchTime * 1000;
+    var lastPos = [self.x, self.y, self.width, self.height];
+    self.setImg(url, lastPos);
+    setTimeout(function () {
+      self.setImg(self.originImgURL, lastPos);
+    }, self.jsonInfo.touchTime);
+  }
+
   setImg(url, pos) {
     var self = this;
-    if (self.img != null) {
+    if (arguments.length == 1) {
+      pos = [self.x, self.y];
+    }
+    if (self.img != null && self.imgReady) {
       self.body.removeChild(this.img);
     }
     var canvas = self.stage.getCanvas();
     self.img = new Image();
+    self.imgReady = false;
     self.img.onload = function () {
+      self.imgReady = true;
       this.style.position = 'absolute';
       this.style.left = self.getCanvas().offsetLeft + pos[0] + 'px';
       this.style.top = self.getCanvas().offsetTop + pos[1] + 'px';
@@ -89,13 +112,15 @@ class Actor {
       this.x = x;
       this.y = y;
       this.img.style.display = '';
-      this.img.style.left = this.getCanvas().offsetLeft + this.x + "px";
-      this.img.style.top = this.getCanvas().offsetTop + this.y + "px";
+      var offsetLeft = this.getCanvas().offsetLeft;
+      var offsetTop = this.getCanvas().offsetTop;
+      this.img.style.left = offsetLeft + this.x + "px";
+      this.img.style.top = offsetTop + this.y + "px";
       this.tracking.moveTo(x, y);
-      return this;
     } else {
       this.moveArray.push([x, y]);
     }
+    return this;
   }
 
   isHide() {
@@ -127,6 +152,7 @@ class Actor {
         x + this.width, y,
         x + this.width, y + this.height,
         x, y + this.height);
+    this.tracking.setFlip(this.isFlip);
     this.tracking.jsonInfo = this.jsonInfo;
     //this.tracking.debug();
     this.tracking.setCvProcess(this.cv.imgFilter);
@@ -142,18 +168,16 @@ class Actor {
 
   start() {
     var self = this;
-    self.stage.onCanvas(function (c) {
-      self.showTime();
-    });
-
-    setTimeout(function () {
-      console.log("Actor start..");
+    self.stage.onReady(function () {
+      //console.log("stage ready , Actor start..");
       self.tracking.start();
       self.running = true;
       for (var i = 0; i < self.moveArray.length; i++) {
         self.moveTo(self.moveArray.shift());
       }
-    }, 1000);
+    });
+    self.stage.onCanvas(function (c) {
+      self.showTime();
+    });
   }
-
 }
